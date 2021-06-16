23ae3f07c63ed9520e01a12a157021b173d65e52
Harshdeep Singh <46198431+Harshdeep-Singh69@users.noreply.github.com>
2021-06-16 17:33:29 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Validation from './Components/Validation';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Validation />
+    </div>
+  );
+}
+
+export default App;
